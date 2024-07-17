9ce7b3a4902e51d2d906367e14c554e091d23231
Eduardo Di Lauro <elauro@magnasistemas.com.br>
2024-07-17 13:12:02 -0300

    feat: layout six

--- app/components/layoutCounter/index.tsx
+++ app/components/layoutCounter/index.tsx
@@ -1,10 +1,11 @@
 import { FivePlayers } from "./fivePlayers";
+import { SixPlayers } from "./sixPlayers";
 import { Props } from "./types";
 
 export const LayoutCounter = ({ players = 2 }: Props) => {
   return (
     <>
-      <FivePlayers />
+      <SixPlayers />
     </>
   );
 };
